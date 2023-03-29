bb8fafbe31b9c0d5afb3f017915f87c4bfe3642b
SankarGanesh-18 <122220555+SankarGanesh-18@users.noreply.github.com>
2023-03-29 12:15:52 +0530

    Add files via upload

---
+++ Smile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Smile() {
+  return (
+    <div>
+        <h1>Smile Component</h1>
+        <p>It is a functional Component</p>
+        <img style={{width:'300px',height:'300px'}}src='https://i.pinimg.com/originals/39/78/06/397806fecb6e1432d8a58f6268976434.jpg' alt=''/>
+    </div>
+  )
+}
+
+export default Smile
